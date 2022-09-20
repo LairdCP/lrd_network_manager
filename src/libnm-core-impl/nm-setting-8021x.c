5334df11d3c4b4ca2533cda7bbd0505fc1c2635f
Erik Strack <erik.strack@lairdconnect.com>
2022-09-20 15:32:25 -0400

    lrd-network-manager: Remove irrelevant comment line "Since: 1.8".
    Bug 22635

--- src/libnm-core-impl/nm-setting-8021x.c
+++ src/libnm-core-impl/nm-setting-8021x.c
@@ -4761,7 +4761,6 @@ nm_setting_802_1x_class_init(NMSetting8021xClass *klass)
      *
      * Flags indicating how to handle the #NMSetting8021x:pac-file-password property.
      *
-     * Since: 1.8
      **/
     /* ---ifcfg-rh---
      * ---end---
